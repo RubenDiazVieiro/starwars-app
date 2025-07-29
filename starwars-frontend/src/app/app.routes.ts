1b80089496bac27b839eaa0f15fc471cce935e83
Ruben Diaz <rubendv@marosavat.com>
2025-07-29 17:01:57 +0200

    [master] add routing

--- starwars-frontend/src/app/app.routes.ts
+++ starwars-frontend/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { PlanetsComponent } from './components/planets/planets.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: '**', redirectTo: '' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // ✅ Redirige / a /home
+  { path: 'home', component: WelcomeComponent }, // ✅ /home muestra Welcome
+  { path: 'characters', component: CharactersComponent },
+  { path: 'planets', component: PlanetsComponent },
+  { path: '**', redirectTo: '/home' } // ✅ Cualquier ruta inválida va a /home
 ];
---
+++ starwars-frontend/src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss'
+})
+export class WelcomeComponent {
+}
